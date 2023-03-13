33ea85b482e308427bffaaba8fcb242f25f09dcb
dehcanuto <andrecanutoc@gmail.com>
2023-03-13 14:58:35 -0300

    tailwind install

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,13 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Homepage</h1>
+      <main>
+        <h1>Homepage</h1>
       </main>
     </div>
   );
--- pages/single/index.jsx
+++ pages/single/index.jsx
@@ -1,15 +1,14 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function SingleHero() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Single</h1>
+      <main>
+        <h1>Single</h1>
       </main>
     </div>
   );
